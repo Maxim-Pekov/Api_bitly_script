ba4e8ec459e29ec4c7f48470000e01a97b0ad7aa
Maxim-Pekov <max.pekov@gmail.com>
2022-08-01 22:18:25 +0300

    get try/except to requests to API

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 
 token = 'c6d39b44191b42c930dcacd0e008ec335fbb0f20'
+
+
 # url = 'https://api-ssl.bitly.com/v4/bitlinks'
 
 def shorten_link(token, url):
@@ -13,16 +15,25 @@ def shorten_link(token, url):
 def main():
     print('Введите ссылку которую хотите уменьшить:')
     url = input()
+    # bitlink = ''
+    try:
+        bitlink = shorten_link(token, url)
+        print('Битлинк', bitlink)
 
-    # shorten_link(token, url)
-    print('Битлинк', shorten_link(token, url))
-    # url2 = 'https://api-ssl.bitly.com/v4/user'
-    #
-    # headers = {
-    #     'Authorization': 'Bearer c6d39b44191b42c930dcacd0e008ec335fbb0f20'
-    # }
-    # response = requests.get(url, headers=headers)
-    # response.raise_for_status()
+    except requests.exceptions.MissingSchema:
+        print('Вы ввели неправильный URL')
+    except requests.exceptions.HTTPError as error:
+        exit("Can't get data from server:\n{0}".format(error))
+
+
+# shorten_link(token, url)
+# url2 = 'https://api-ssl.bitly.com/v4/user'
+#
+# headers = {
+#     'Authorization': 'Bearer c6d39b44191b42c930dcacd0e008ec335fbb0f20'
+# }
+# response = requests.get(url, headers=headers)
+# response.raise_for_status()
 
 
 if __name__ == '__main__':
