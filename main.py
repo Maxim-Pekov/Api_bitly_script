461ba6e71dc8866c391b468a90406bcf10cb7696
Maxim-Pekov <max.pekov@gmail.com>
2022-08-01 21:05:28 +0300

    create shorten_link function

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+
+token = 'c6d39b44191b42c930dcacd0e008ec335fbb0f20'
+url = 'https://api-ssl.bitly.com/v4/bitlinks'
+
+
+def shorten_link(token, url):
+    headers = {'Authorization': f'Bearer {token}'}
+    body = {"long_url": "https://github.com/maxim-pekov"}
+    response2 = requests.post(url, json=body, headers=headers)
+    return response2.json()['link']
+
+
+def main():
+    # shorten_link(token, url)
+    print('Битлинк', shorten_link(token, url))
+    # url2 = 'https://api-ssl.bitly.com/v4/user'
+    #
+    # headers = {
+    #     'Authorization': 'Bearer c6d39b44191b42c930dcacd0e008ec335fbb0f20'
+    # }
+    # response = requests.get(url, headers=headers)
+    # response.raise_for_status()
+
+
+if __name__ == '__main__':
+    main()
