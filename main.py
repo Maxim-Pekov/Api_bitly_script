e3575e613dfa3cb939c8f781bc7e71a2b17f1b2c
Maxim-Pekov <max.pekov@gmail.com>
2022-08-02 15:07:03 +0300

    put all prints into ifmain function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 import os
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 TOKEN = os.getenv('token')
-
+#       https://bit.ly/3JkG0mU
 
 def shorten_link(token, url):
     api_url = 'https://api-ssl.bitly.com/v4/bitlinks'
@@ -15,7 +16,9 @@ def shorten_link(token, url):
 
 
 def total_clicks(token, bitlink):
-    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+    url = urlparse(bitlink)
+    short_bitlink = url.netloc + url.path
+    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{short_bitlink}/clicks/summary'
     headers = {'Authorization': f'Bearer {token}'}
     response = requests.get(api_url, headers=headers)
     response.raise_for_status()
@@ -25,27 +28,25 @@ def total_clicks(token, bitlink):
 def is_bitlink(TOKEN, url):
     if 'bit.ly' in url:
         try:
-            t = total_clicks(TOKEN, url)
-            print(f'По вашей ссылке прошли {t} раз(а)')
-            # print(f'По вашей ссылке прошли {t["total_clicks"]} раз(а)')
+            count_clicks = total_clicks(TOKEN, url)
         except requests.exceptions.HTTPError as error:
             exit("Can't get data from server:\n{0}".format(error))
+        return f'По вашей ссылке прошли {count_clicks} раз(а)'
     else:
         try:
             bitlink = shorten_link(TOKEN, url)
-            print('Битлинк', bitlink)
         except requests.exceptions.MissingSchema:
-            print('Вы ввели неправильный URL')
+            return 'Вы ввели неправильный URL'
         except requests.exceptions.HTTPError as error:
             exit("Can't get data from server:\n{0}".format(error))
-
+        return f'Битлинк {bitlink}'
 
 def main():
     load_dotenv()
     TOKEN = os.getenv('token')
-    print('Введите ссылку:')
-    url = input()
-    is_bitlink(TOKEN, url)
+    # print('Введите ссылку:''Введите ссылку:')
+    url = input('Введите ссылку:').strip()
+    print(is_bitlink(TOKEN, url))
 
 
 if __name__ == '__main__':
